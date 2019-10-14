be96f7233bc75cf69396261c6375fb4ba41f1e05
lkorba <lukasz.korbasiewicz@gmail.com>
2019-10-14 13:49:40 +0000

    changed sublist3r to amass, added dnsgen

--- enum.sh
+++ enum.sh
@@ -9,23 +9,15 @@ fi
 echo 'Creating project '$1
 mkdir ~/$1
 cd ~/$1
-mkdir ~/$1/sublist3r
 mkdir ~/$1/screenshots
 mkdir ~/$1/html
 mkdir ~/$1/gobuster
 
 echo 'Enumerating first level domain'
-sublist3r -d $1 -o ~/$1/$1.sub1.txt
+amass enum --passive -d $1 -o ~/$1/$1.amass.txt
 
-echo 'Enumerating second level domains'
-while read line  ; do sublist3r -d $line -o ~/$1/sublist3r/$line.txt ; done < ~/$1/$1.sub1.txt
-
-echo 'Compile all subdomains into one file'
-cat ~/$1/sublist3r/*.txt | sort | uniq > ~/$1/$1.sub2.txt
-cat ~/$1/*.sub*.txt | sort | uniq > ~/$1/$1.alldomains.txt
-
-echo 'Checking what resolves with massdns'
-~/tools/massdns/bin/massdns ~/$1/$1.alldomains.txt -r ~/tools/massdns/lists/resolvers.txt -o S -w ~/$1/$1.mdns.txt
+echo 'Generating more domain names and checking what resolves with massdns'
+cat ~/$1/$1.amass.txt | dnsgen - | ~/tools/massdns/bin/massdns -r ~/tools/massdns/lists/resolvers.txt -o S -w ~/$1/$1.mdns.txt
 
 echo 'Sort, uniq, extract first column (domain names) and remove dot from the end of a file name'
 awk '{print $1}' ~/$1/$1.mdns.txt |sort|uniq|sed 's/\.$//' > ~/$1/$1.resolved.txt
